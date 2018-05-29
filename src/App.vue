0c8e4187ee94ac48489bff97370fca1782d125d3
liulianxing <liulianxing@meituan.com>
2018-05-29 16:45:08 +0800

    成了

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
+
 <script>
+console.log(1111);
 export default {
-  name: 'App'
-}
+    name: "App"
+};
 </script>
 
 <style lang="stylus">
-#app
-    font-family: 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    text-align: center
-    color: #2c3e50
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
 </style>
